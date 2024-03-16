81d23f11bdadac5cb73ed37f8029ac33b7148c5b
Amoux98021 <107431774+Amoux98021@users.noreply.github.com>
2024-03-16 01:07:18 -0400

    Update1 project1_parser.py

--- project1_parser.py
+++ project1_parser.py
@@ -1,11 +1,100 @@
 # Lexer
+class Token:
+    def __init__(self, type, value=None):
+        self.type = type
+        self.value = value
+
+    def __repr__(self):
+        return f"Token({self.type}, {self.value})"
+
 class Lexer:
     def __init__(self, code):
         self.code = code
         self.position = 0
+        self.current_char = self.code[self.position]
+
+    def advance(self):
+        self.position += 1
+        if self.position >= len(self.code):
+            self.current_char = None  # Indicates end of input
+        else:
+            self.current_char = self.code[self.position]
+
+    def skip_whitespace(self):
+        while self.current_char is not None and self.current_char.isspace():
+            self.advance()
+
+    def number(self):
+        result = ''
+        while self.current_char is not None and self.current_char.isdigit():
+            result += self.current_char
+            self.advance()
+        return int(result)
+
+    def identifier(self):
+        result = ''
+        while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
+            result += self.current_char
+            self.advance()
+        return result
 
     # move the lexer position and identify next possible tokens.
     def get_token(self):
+        # Skip any whitespace
+        self.skip_whitespace()
+
+        # End of input
+        if self.current_char is None:
+            return Token('EOF', None)
+
+        # Number token
+        if self.current_char.isdigit():
+            return Token('NUMBER', self.number())
+
+        # Identifier or keyword
+        if self.current_char.isalpha():
+            ident = self.identifier()
+            # Check if the identifier matches any keyword
+            if ident in ('if', 'then', 'else', 'while', 'do'):
+                return Token('KEYWORD', ident)
+            else:
+                return Token('VARIABLE', ident)
+
+        # Operators
+        if self.current_char in ('+', '-', '*', '/', '(', ')', '=', '>', '<', '!', ';'):
+            # Handle multi-character operators
+            if self.current_char == '=' and self.peek() == '=':
+                self.advance()
+                self.advance()
+                return Token('OPERATOR', '==')
+            elif self.current_char == '!' and self.peek() == '=':
+                self.advance()
+                self.advance()
+                return Token('OPERATOR', '!=')
+            elif self.current_char == '>' and self.peek() == '=':
+                self.advance()
+                self.advance()
+                return Token('OPERATOR', '>=')
+            elif self.current_char == '<' and self.peek() == '=':
+                self.advance()
+                self.advance()
+                return Token('OPERATOR', '<=')
+            else:
+                # Single character operators and parentheses
+                char = self.current_char
+                self.advance()
+                return Token('OPERATOR' if char in ('+', '-', '*', '/', '=', '>', '<') else 'PAREN', char)
+
+        # If we reach here, it means we encountered an unknown character
+        raise Exception(f"Unknown character: {self.current_char}")
+
+    def peek(self):
+        # Look at the next character without consuming it
+        peek_pos = self.position + 1
+        if peek_pos >= len(self.code):
+            return None
+        return self.code[peek_pos]
+
 
 # Parser
 # Input : lexer object
