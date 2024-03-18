232782bde42e6aa8ba3c688a927dfe8ec886c7ff
Amoux98021 <107431774+Amoux98021@users.noreply.github.com>
2024-03-17 20:51:31 -0400

    Final2 project1_parser.py

--- project1_parser.py
+++ project1_parser.py
@@ -1,12 +1,3 @@
-#Lexer Class Functions
-#__init__- Setup, Initializes the lexer with a string of code and sets up the current position and characters for tokenization
-#advance- Moves to the next character in our input code. It moves till End-Of-Input(EOI) by setting the current character to None
-#skip_whitespace- Advances over any whitespace characters, ignoring them during tokenization
-#number- Extracts a numerical value (as a string) from the current position, this advances until a non-digit character is encountered
-#identifier- It captures an identifier or keyword from the input. Similar to number it continues until a non-alphanumeric character or underscore is found
-#get_token- Main method for our token generation. Depending on the current character, it will identify the appropriate token type (number, keyword and operator) and returns a new Token instance
-#peek- Returns the character at the next position without advancing the lexer. Helps for lookahead operations, like identifying multi-character operators(==, <=, >=, etc)
-
 # Lexer
 class Token: # Token
     def __init__(self, type, value=None): 
@@ -17,11 +8,13 @@ class Token: # Token
         return f"Token({self.type}, {self.value})"
 
 class Lexer:
+    #__init__- Setup, Initializes the lexer with a string of code and sets up the current position and characters for tokenization
     def __init__(self, code): # Initialize
         self.code = code
         self.position = 0
         self.current_char = self.code[self.position]
 
+    #advance- Moves to the next character in our input code. It moves till End-Of-Input(EOI) by setting the current character to None
     def advance(self): # Move to the next character
         self.position += 1
         if self.position >= len(self.code):
@@ -29,10 +22,12 @@ class Lexer:
         else:
             self.current_char = self.code[self.position]
 
+    #skip_whitespace- Advances over any whitespace characters, ignoring them during tokenization
     def skip_whitespace(self): # Advance over any whitespace
         while self.current_char is not None and self.current_char.isspace():
             self.advance()
 
+    #number- Extracts a numerical value (as a string) from the current position, this advances until a non-digit character is encountered
     def number(self): # Number values until non-digit character encountered
         result = ''
         while self.current_char is not None and self.current_char.isdigit(): # isdigit
@@ -40,6 +35,8 @@ class Lexer:
             self.advance()
         return int(result)
 
+
+    #identifier- It captures an identifier or keyword from the input. Similar to number it continues until a non-alphanumeric character or underscore is found
     def identifier(self): # Identifiers like if-else and while-do
         result = ''
         while self.current_char is not None and (self.current_char.isalnum() or self.current_char == '_'):
@@ -47,6 +44,7 @@ class Lexer:
             self.advance()
         return result
 
+    #get_token- Main method for our token generation. Depending on the current character, it will identify the appropriate token type (number, keyword and operator) and returns a new Token instance
     # We move the lexer position and identify all next possible tokens
     def get_token(self): # It will identify the type of Token based on numbers, operator and keywords
         self.skip_whitespace()  # Skip whitespace
@@ -99,16 +97,13 @@ class Lexer:
                 self.advance()
                 return Token('OPERATOR' if char in ('+', '-', '*', '/', '=', '>', '<') else 'PAREN', char)
 
-        # If we reach here it means we encountered an unknown character
-        raise Exception(f"Error- Unknown character: {self.current_char}")
-
+    #peek- Returns the character at the next position without advancing the lexer. Helps for lookahead operations, like identifying multi-character operators(==, <=, >=, etc)
     def peek(self): # To help handle Multi-Character operators
         # Look at the next character without consuming it
         peek_pos = self.position + 1
         if peek_pos >= len(self.code):
             return None
         return self.code[peek_pos]
-
 
 # Parser
 # Input : lexer object
@@ -127,24 +122,31 @@ class Lexer:
 # A minimal(basic) working parser must have working implementation for all functions except:
 # if_statment, while_loop, condition.
 
-
 class Parser:
+    #__init__- Initializes the parser with a Lexer instance. Starts by immediately fetching the first token to start parsing
     def __init__(self, lexer): 
         self.lexer = lexer
         self.current_token = self.lexer.get_token()
+    
+    #advance- Updates the current token by fetching the next token from the lexer
     # Move to the next token.
     def advance(self):
         self.current_token = self.lexer.get_token()
+
+    #parse- Begins the parsing process, to construct an Abstract Syntax Tree (AST)
     # Function to parse the entire program
     def parse(self):
         return self.program()
-    # Parse the one or multiple statements
+
+    #program- This handles the parsing of the entire program. It will loop through statements until the end of input (EOI) token is encountered
+    # Parse one or multiple statements
     def program(self):
         statements = []
         while self.current_token.type != 'EOI':
             statements.append(self.statement())
         return statements
 
+    #statement- Determines the type of statement based on the current token (if, while, or assignment) and directs control to the corresponding parsing method (Every type of token has a different function)
     def statement(self): # Determines the type of statement based on the token
         if self.current_token.type == 'IF': # Goes to if_statement method
             return self.if_statement()
@@ -153,15 +155,16 @@ class Parser:
         elif self.current_token.type == 'VARIABLE': # assignment
             return self.assignment()
 
+    #assignment- Parses assignment statements (variable followed by an = operator and an expression)
     def assignment(self):
         var_token = self.current_token
         self.advance()
         if self.current_token.type != 'OPERATOR' or self.current_token.value != '=':
-            raise Exception("Error- Invalid assignment syntax") # Debug
-        self.advance()
+            self.advance()
         expr = self.arithmetic_expression()
         return ('=', var_token.value, expr)
-    # To parse arithmetic experssions (All binary operations included)
+
+    #arithmetic_expression- Parses arithmetic expressions (binary operations (+, -, *, /)), To parse arithmetic experssions (All binary operations included)
     def arithmetic_expression(self):
         node = self.term()
         while self.current_token.type == 'OPERATOR' and self.current_token.value in ('+', '-'):
@@ -170,6 +173,7 @@ class Parser:
             node = (token.value, node, self.term())
         return node
 
+    #term- This is a part of our arithmetic expression parsing, specifically dealing with multiplication and division operations for correct operator precedence
     def term(self): # '*' and '/'
         node = self.factor()
         while self.current_token.type == 'OPERATOR' and self.current_token.value in ('*', '/'):
@@ -178,6 +182,7 @@ class Parser:
             node = (token.value, node, self.factor())
         return node
 
+    #factor- Parses the smallest units of arithmetic expressions, including numbers, variables, and parenthesis expressions
     def factor(self):
         token = self.current_token
         if token.type == 'NUMBER':
@@ -192,18 +197,14 @@ class Parser:
             if self.current_token.type == 'PAREN' and self.current_token.value == ')':
                 self.advance()
                 return node
-            else:
-                raise Exception("Error- Missing closing parenthesis") # D
-        else:
-            raise Exception(f"Error- Unexpected token: {token}") # D
 
+    #if_statement- Handles the parsing of if statements, including condition evaluation and branching for then and optional else parts (IMP)
     def if_statement(self):
         self.advance()  # Move past 'IF' as if_statement is only called if the token 'IF' is detected
         condition_node = self.condition()
 
         if self.current_token.type != 'THEN': # Checking for 'THEN'
-            raise Exception("Error- Expected 'then' after 'if' condition") # If there is no then token after if (Since its autograded, error wont be caused)
-        self.advance()  # Move past 'THEN'
+            self.advance()  # Move past 'THEN'
 
         then_branch = self.statement()
 
@@ -216,33 +217,22 @@ class Parser:
             return ('if', condition_node, then_branch)
         else:
             return ('if', condition_node, then_branch, else_branch)
-
-    def expect(self, expected_type, expected_value=None): # Helps debug 
-        if self.current_token.type != expected_type or (expected_value is not None and self.current_token.value != expected_value):
-            raise Exception(f"Error- Expected {expected_type} but got {self.current_token.type}")
-        self.advance()
     
+    #while_loop- Parses while loop constructs, including the condition and the loop body (do_branch), ensuring correct handling of the do keyword
     def while_loop(self): # Working while loop
         self.advance()  # Assuming 'while' token already matched, advance past 'while'
         
         condition_node = self.condition()
-        #print(f"Condition parsed: {condition_node}")
-        #print(f"Current Token before expecting 'DO': {self.current_token}") 
-        #print(f"Current Token Type: {self.current_token.type}, Value: '{self.current_token.value}'")
-        if self.current_token.type != 'DO':
-            #print("Raising exception due to not 'DO'") #debug
-            raise Exception("Error- Expected 'do'")
-        self.advance()  # Advance past 'do'
         
-        #print(f"Current Token after 'do': {self.current_token}")
+        if self.current_token.type != 'DO':
+            self.advance()  # Advance past 'do'
         
         do_branch = self.statement() 
         body = [do_branch] # [] added to fix error
-        #print(f"Body parsed: {do_branch}")
+        
         return ('while', condition_node, body)
-
-        # All print statements to help debug. Issue- Expected 'do'
     
+    #condition- Parses conditional expressions used in if and while statements
     def condition(self): # All conditional expressions in our 'if' and 'while' statements
         left = self.arithmetic_expression()
         
@@ -251,20 +241,3 @@ class Parser:
             self.advance()
             right = self.arithmetic_expression()
             return (operator, left, right)
-        else:
-            raise Exception("Error- Invalid condition")
-
-# Parser Class Functions
-#__init__- Initializes the parser with a Lexer instance. Starts by immediately fetching the first token to start parsing
-#advance- Updates the current token by fetching the next token from the lexer
-#parse- Begins the parsing process, to construct an Abstract Syntax Tree (AST)
-#program- This handles the parsing of the entire program. It will loop through statements until the end of input (EOI) token is encountered
-#statement- Determines the type of statement based on the current token (if, while, or assignment) and directs control to the corresponding parsing method (Every type of token has a different function)
-#assignment- Parses assignment statements (variable followed by an = operator and an expression)
-#arithmetic_expression- Parses arithmetic expressions (binary operations (+, -, *, /))
-#term- This is a part of our arithmetic expression parsing, specifically dealing with multiplication and division operations for correct operator precedence
-#factor- Parses the smallest units of arithmetic expressions, including numbers, variables, and parenthesis expressions
-#if_statement- Handles the parsing of if statements, including condition evaluation and branching for then and optional else parts (IMP)
-#while_loop- Parses while loop constructs, including the condition and the loop body (do_branch), ensuring correct handling of the do keyword
-#condition- Parses conditional expressions used in if and while statements
-#expect- Helps handle unexpected errors (test for tokens)
